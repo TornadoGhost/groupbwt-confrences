9b6a4a2040d360bd9c1f82a64297691c08606f4b
tornadoghost <tornadoghost97@gmail.com>
2024-12-16 15:41:00 +0200

    DCTN-916: added redirect if user authenticated

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,12 +13,19 @@ class LoginController extends AbstractController
     /**
      * @Route(path="/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+        if ($this->getUser()) {
+            $referer = $request->headers->get('referer');
 
-        // last username entered by the user
+            if ($referer) {
+                return $this->redirect($referer);
+            } else {
+                return $this->redirectToRoute('temp_page');
+            }
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/login.html.twig', [
