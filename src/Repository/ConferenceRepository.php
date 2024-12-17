7d457573850d0c479c19e9f78d22eb156728a17c
tornadoghost <tornadoghost97@gmail.com>
2024-12-17 13:57:02 +0200

    DCTN-919: added methods

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -3,8 +3,12 @@
 namespace App\Repository;
 
 use App\Entity\Conference;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @extends ServiceEntityRepository<Conference>
@@ -19,5 +23,48 @@ class ConferenceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conference::class);
+    }
+
+    public function getAllConferencesWithSpecificUser($userId): QueryBuilder
+    {
+        if (!$userId) {
+            return $this->createQueryBuilder('c')
+                ->select('c, u')
+                ->leftJoin('c.users', 'u')
+                ->where("c.deletedAt IS NULL");
+        }
+        return $this->createQueryBuilder('c')
+            ->select('c, u')
+            ->leftJoin('c.users', 'u', Join::WITH, 'u.id = :userId')
+            ->where("c.deletedAt IS NULL")
+            ->setParameter('userId', $userId);
+    }
+
+    public function workWithConference($job, Conference $conference, User $user): void
+    {
+        if ($job === 'add') {
+            $conference->addUser($user);
+        } else if ($job === 'remove') {
+            $conference->removeUser($user);
+        }
+
+        $this->_em->persist($conference);
+        $this->_em->flush();
+    }
+
+    /**
+     * @param User|UserInterface $user
+     */
+    public function addUserToConference(Conference $conference, $user): void
+    {
+        $this->workWithConference('add', $conference, $user);
+    }
+
+    /**
+     * @param User|UserInterface $user
+     */
+    public function removeUserFromConference(Conference $conference, $user): void
+    {
+        $this->workWithConference('remove', $conference, $user);
     }
 }
