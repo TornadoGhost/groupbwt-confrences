6733bc91522d1d722b2753b9814be3ff7fdb35f8
tornadoghost <tornadoghost97@gmail.com>
2024-12-17 13:47:53 +0200

    DCTN-919: added migration

---
+++ migrations/Version20241217090005.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241217090005 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX `primary` ON user_conference');
+        $this->addSql('ALTER TABLE user_conference ADD PRIMARY KEY (conference_id, user_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX `PRIMARY` ON user_conference');
+        $this->addSql('ALTER TABLE user_conference ADD PRIMARY KEY (user_id, conference_id)');
+    }
+}
