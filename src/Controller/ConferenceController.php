9d2b4b8c7f618a215867c5a0659889e324195960
tornadoghost <tornadoghost97@gmail.com>
2024-12-17 13:52:08 +0200

    DCTN-919: changed group route name

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/conference")
+ * @Route("/conferences")
  */
 class ConferenceController extends AbstractController
 {
