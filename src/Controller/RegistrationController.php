4912a4ab23642b5f86662f2094645c131edee0f1
tornadoghost <tornadoghost97@gmail.com>
2024-12-18 13:01:34 +0200

    DCTN-921: moved logic to the service class

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\RegistrationService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,26 +12,20 @@ class RegistrationController extends BaseAuthController
     /**
      * @Route(path="/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request                     $request,
+        RegistrationService         $registrationService
+    ): Response
     {
         if ($this->getUser()) {
             return $this->refererRedirect($request);
         }
 
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+        $user = $registrationService->createNewUser();
+        $form = $registrationService->userFormPrep($request, $user);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $registrationService->saveNewUser($user, $form);
 
             return $this->redirectToRoute('app_conference_index');
         }
