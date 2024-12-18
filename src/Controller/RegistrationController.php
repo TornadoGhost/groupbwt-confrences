300b198f49fdb608f1dee8fd41ba3a864e4280a3
tornadoghost <tornadoghost97@gmail.com>
2024-12-18 13:00:19 +0200

    DCTN-921: replaced AbstractController with BaseAuthController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,10 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegistrationController extends AbstractController
+class RegistrationController extends BaseAuthController
 {
     /**
      * @Route(path="/register", name="app_register")
