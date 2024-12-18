1829ed99bc073cf069cacaf04778d541606287b8
tornadoghost <tornadoghost97@gmail.com>
2024-12-18 13:02:04 +0200

    DCTN-921: added validation to form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,8 +12,10 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
 
 class RegistrationFormType extends AbstractType
 {
@@ -23,6 +25,20 @@ class RegistrationFormType extends AbstractType
             ->add('email', null, [
                 'attr' => [
                     'placeholder' => 'name@example.com'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The email should be not empty',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'The email should be at least {{ limit }} characters',
+                        'max' => 100,
+                        'maxMessage' => 'The email should be not longer than {{ limit }} characters'
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email address'
+                    ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
@@ -47,27 +63,80 @@ class RegistrationFormType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Firstname example',
                 ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The firstname should be not empty',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'The firstname should be at least {{ limit }} characters',
+                        'max' => 100,
+                        'maxMessage' => 'The firstname should be not longer than {{ limit }} characters'
+                    ]),
+                ],
             ])
             ->add('lastname', TextType::class, [
                 'attr' => [
                     'placeholder' => 'Lastname example'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The lastname should be not empty',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'The lastname should be at least {{ limit }} characters',
+                        'max' => 100,
+                        'maxMessage' => 'The lastname should be not longer than {{ limit }} characters'
+                    ]),
+                ],
             ])
             ->add('birthdate', BirthdayType::class, [
                 'widget' => 'single_text',
                 'format' => 'yyyy-MM-dd',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Start date cannot be blank.',
+                    ]),
+                    new Type([
+                        'type' => \DateTimeInterface::class,
+                        'message' => 'The value {{ value }} is not a valid date.',
+                    ]),
+                ]
             ])
-            ->add('country', CountryType::class)
+            ->add('country', CountryType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please select a country.',
+                    ])
+                ]
+            ])
             ->add('phone', TextType::class, [
                 'label' => 'Phone number',
                 'attr' => [
                     'class' => 'phone-input'
-                ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The phone number should be not empty',
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'The phone number should be at least {{ limit }} characters',
+                        'max' => 20,
+                        'maxMessage' => 'The phone number should be not longer than {{ limit }} characters'
+                    ]),
+                ],
             ])
             ->add('type', EntityType::class, [
-                'class' => Type::class,
+                'class' => UserType::class,
                 'choice_label' => 'name',
                 'placeholder' => 'Select type',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please select a user type.',
+                    ])
+                ],
             ])
         ;
     }
