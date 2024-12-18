2b487010c9e0c1c82718b258c684e9fe7f8f9848
tornadoghost <tornadoghost97@gmail.com>
2024-12-18 13:02:17 +0200

    DCTN-921: added registration service

---
+++ src/Service/RegistrationService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class RegistrationService
+{
+    private $formFactory;
+    private $userRepository;
+    private $userPasswordHasher;
+
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        UserRepository  $userRepository,
+        UserPasswordHasherInterface $userPasswordHasher,
+    )
+    {
+        $this->formFactory = $formFactory;
+        $this->userRepository = $userRepository;
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    public function userFormPrep(Request $request, UserInterface $user): FormInterface
+    {
+        $form = $this->formFactory->create(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        return $form;
+    }
+
+    public function createNewUser(): User
+    {
+        return $this->userRepository->newUser();
+    }
+
+    public function saveNewUser(UserInterface $user, FormInterface $form)
+    {
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            )
+        );
+        $getUserTypeName = $user->getType()->getName();
+
+        $user->setRoles(['ROLE_' . strtoupper($getUserTypeName)]);
+
+        $this->userRepository->saveUser($user);
+    }
+
+}
