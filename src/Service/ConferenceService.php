5378c1bbdeb619683cea9936576857d40c6f05ae
tornadoghost <tornadoghost97@gmail.com>
2024-12-19 16:40:25 +0200

    DCTN-921: changed method name

--- src/Service/ConferenceService.php
+++ src/Service/ConferenceService.php
@@ -49,7 +49,7 @@ class ConferenceService
         $this->conferenceRepository->removeUserFromConference($conference, $user);
     }
 
-    public function prepareForm(Request $request, Conference $conference, FormInterface $form): FormInterface
+    public function formPreparation(Request $request, Conference $conference, FormInterface $form): FormInterface
     {
         $latitude = $conference->getAddress()[0] ?? null;
         $longitude = $conference->getAddress()[1] ?? null;
