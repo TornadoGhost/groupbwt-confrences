58c482e99ac3eff26d4c74670d3e69d09d7901ce
tornadoghost <tornadoghost97@gmail.com>
2024-12-19 16:41:51 +0200

    DCTN-921: added user entity

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Service\RegistrationService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
